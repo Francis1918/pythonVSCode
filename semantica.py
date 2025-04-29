fb591b089f53c8a0729520a43d67a33c0a92a298
Francis19 <9alemanbravo@gmail.com>
2025-04-29 12:18:01 -0500

    txy

---
+++ semantica.py
@@ -0,0 +1,13 @@
+#uso y semantica en python
+# Definicion de la clase Semantica
+# clase Semantica
+#class Semantica:
+#    def __init__(self):
+#        self.variables = {}  # Diccionario para almacenar variables y sus tipos
+#        self.funciones = {}  # Diccionario para almacenar funciones y sus tipos
+#        self.errores = []    # Lista para almacenar errores semánticos
+#        self.ambito = "global"  # Ambito actual (global o local)
+#        self.tipo_actual = None  # Tipo actual de la expresión
+#        self.tipo_funcion = None  # Tipo de la función actual
+#        self.tipo_variable = None  # Tipo de la variable actual
+#        self.tipo_parametro = None  # Tipo del parámetro actual
